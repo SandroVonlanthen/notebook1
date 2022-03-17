b3940c6751bf62a421804cdd68d9f8225e7200e1
SandroVonlanthen <91312514+SandroVonlanthen@users.noreply.github.com>
2022-03-17 17:46:28 +0100

    ex 4 done

--- exe4.3.py
+++ exe4.3.py
@@ -1,31 +1,27 @@
 
 
 
-
 from urllib import request
 from bs4 import BeautifulSoup
+#
+urlbase = " https://www.indeed.com/q-"
+listjobs= ['teacher','lawyer','data-scientist']
+urlfinisch = "-jobs.html"
+returnDict= {}
 
-
-res = request.urlopen("http://forecast.weather.gov/MapClick.php?lat=21.3049&lon=-157.8579")
-soup = BeautifulSoup(res, 'html.parser')
-
-#temp = soup.find(id_ =  "detailed-forecast-body")
-
-temp = soup.find("div", {"id": "detailed-forecast-body"})
-
-temp1= temp.find_all("div", {"class": "row row-even row-forecast"})
-temp2= temp.find_all("div", {"class": "row row-odd row-forecast"})
-
-
-for i in range(7):
+for elemnts in listjobs:
     
-    quando2 = temp2[i].find(class_ =  "col-sm-2 forecast-label").getText()
-    info2 = temp2[i].find(class_ =  "col-sm-10 forecast-text").getText()
-    print("%s:  %s"%(quando2, info2))
+    url = urlbase+elemnts+urlfinisch
+    res = request.urlopen(url)
+    soup = BeautifulSoup(res, 'html.parser')
+    companyList =[]
+    firstcompany= soup.find_all(class_ ="companyName")
+    for company in firstcompany:
+        companyList. append(company.getText())
+    returnDict[elemnts]= companyList
 
-    quando1 = temp1[i].find(class_ =  "col-sm-2 forecast-label").getText()
-    info1 = temp1[i].find(class_ =  "col-sm-10 forecast-text").getText()
-    print("%s:  %s"%(quando1, info1))
+#print(returnDict)    
 
-
+for key, value in returnDict.items():
+    print(key, ' : ', value)
 
